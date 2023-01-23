da7579c8f1da12dacc8cbf63b0667dd5661b7dc8
ValentinoMartz <valentinomartinez55@gmail.com>
2023-01-23 15:05:04 -0300

    Controllers

---
+++ WSClinica/Controllers/ClinicaController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WSClinica.Data;
+using System.Linq;
+using WSClinica.Models;
+using WSClinica.Migrations;
+
+namespace WSClinica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClinicaController : ControllerBase
+    {
+        private readonly DBWSClinicaContext context;
+
+        public ClinicaController(DBWSClinicaContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Clinica>> Get()
+        {
+            return context.Clinicas.ToList();
+        }
+
+        [HttpGet("{ID}")]
+        public ActionResult<Clinica> GetById(int ID)
+        {
+            Clinica clinica = (from c in context.Clinicas
+                               where c.ID == ID
+                               select c).SingleOrDefault();
+            return clinica;
+        }
+        [HttpPost]
+
+        public ActionResult<Clinica> Post(Clinica clinica)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            context.Clinicas.Add(clinica);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut("{ID}")]
+        public ActionResult<Clinica> Put(int id, [FromBody]Clinica clinica)
+        {
+            if (id != clinica.ID)
+            {
+                return BadRequest();
+            }
+            context.Entry(clinica).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{ID}")]
+        public ActionResult<Clinica> Delete(int id, [FromBody] Clinica clinica)
+        {
+            var clinicaOriginal = (from c in context.Clinicas
+                           where c.ID == id
+                           select c).SingleOrDefault();
+
+            if (clinicaOriginal == null)
+            {
+                return NotFound();
+            }
+            context.Clinicas.Remove(clinicaOriginal);
+            context.SaveChanges();
+            return clinica;
+        }
+
+    }
+}
--- WSClinica/Data/DBWSClinicaContext.cs
+++ WSClinica/Data/DBWSClinicaContext.cs
@@ -5,13 +5,12 @@ namespace WSClinica.Data
 {
     public class DBWSClinicaContext : DbContext
     {
-       // public DBLibrosBootcampContext(DbContextOptions<DBLibrosBootcampContext> options) : base(options) { }
         public DBWSClinicaContext(DbContextOptions<DBWSClinicaContext> options) : base(options) { }
 
-        DbSet<Clinica> Clinicas { get; set; }
-        DbSet<Habitacion> Habitaciones { get; set; }
-        DbSet<Medico> Medicos { get; set; }
-        DbSet<Paciente> Pacientes { get; set; }
-        DbSet<Especialidad> Especialidades { get; set; }
+        public DbSet<Clinica> Clinicas { get; set; }
+        public DbSet<Habitacion> Habitaciones { get; set; }
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Especialidad> Especialidades { get; set; }
     }
 }
