72266c0bb70a25ff35f1bef5f45bd664df49a6b4
Graciana Amaranto <xenrelay333@hotmail.com>
2023-01-23 15:33:37 -0300

    Medico agregado

--- WSClinica/Controllers/MedicosController.cs
+++ WSClinica/Controllers/MedicosController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using WSClinica.Controllers;
+using WSClinica.Data;
+using WSClinica.Models;
 
 namespace WSClinica.Controllers
 {
@@ -7,5 +13,66 @@ namespace WSClinica.Controllers
     [ApiController]
     public class MedicosController : ControllerBase
     {
+        private readonly DBWSClinicaContext context;
+
+        public MedicosController(DBWSClinicaContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Medico>> Get()
+        {
+            return context.Medicos.ToList();
+        }
+
+        [HttpGet("{IdMedico}")]
+        public ActionResult<Medico> GetById(int IdMedico)
+        {
+            Medico medico = (from m in context.Medicos
+                            where m.IdMedico == IdMedico
+                            select m).SingleOrDefault();
+            return medico;
+        }
+
+        [HttpPost]
+        public ActionResult<Medico> Post(Medico medico)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            context.Medicos.Add(medico);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut("{IdMedico}")]
+        public ActionResult<Medico> Put(int IdMedico, [FromBody] Medico medico)
+        {
+            if (IdMedico != medico.IdMedico)
+            {
+                return BadRequest();
+            }
+            context.Entry(medico).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{IdMedico}")]
+        public ActionResult<Medico> Delete (int IdMedico, [FromBody] Medico medico) 
+        {
+            var medicoOriginal = (from m in context.Medicos
+                                  where m.IdMedico == IdMedico
+                                  select m).SingleOrDefault();
+            if (medicoOriginal == null)
+            {
+                return NotFound();
+                
+            }
+            context.Medicos.Remove(medicoOriginal);
+            context.SaveChanges();
+            return medico;
+        }
     }
 }
